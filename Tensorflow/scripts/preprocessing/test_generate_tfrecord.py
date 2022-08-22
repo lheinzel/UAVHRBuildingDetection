349ff9be575e33b0d7362651f686f2e80897eb96
lheinzel <linus.heinzelmann@uni-ulm.de>
2022-08-22 17:06:33 +0200

    Removed test lines in generate_tfrecord.py added code to generate the tfrecord for the training partition to test_generate_tfrecord.py

--- Tensorflow/scripts/preprocessing/test_generate_tfrecord.py
+++ Tensorflow/scripts/preprocessing/test_generate_tfrecord.py
@@ -1,10 +1,30 @@
 import subprocess
 
-xmldir = r"Tensorflow/workspace/images/test"
-labelsPath = r"Tensorflow/workspace/training_Demo/annotations/labelmap.pbtxt"
-outputPath = r"Tensorflow/workspace/training_Demo/annotations/test.record"
-imageDir = r"Tensorflow/workspace/images/test"
+# Paths for Test partition
+xmldirTest = r"Tensorflow/workspace/images/test"
+labelsPathTest = r"Tensorflow/workspace/training_Demo/annotations/labelmap.pbtxt"
+outputPathTest = r"Tensorflow/workspace/training_Demo/annotations/test.record"
+imageDirTest = r"Tensorflow/workspace/images/test"
 
-cmd = "py Tensorflow/scripts/preprocessing/generate_tfrecord.py -x" + xmldir + " -l " + labelsPath + " -o " + outputPath + " -i " + imageDir
+# Create tfrecord for test patition
+cmd = "py Tensorflow/scripts/preprocessing/generate_tfrecord.py -x" + xmldirTest + " -l " + labelsPathTest
+cmd += " -o " + outputPathTest + " -i " + imageDirTest
+p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell = True)
+out, err = p.communicate()
+print(out)
+print(err)
+
+# Paths for Training partition
+xmldirTrain = r"Tensorflow/workspace/images/train"
+labelsPathTrain = r"Tensorflow/workspace/training_Demo/annotations/labelmap.pbtxt"
+outputPathTrain = r"Tensorflow/workspace/training_Demo/annotations/train.record"
+imageDirTrain = r"Tensorflow/workspace/images/train"
+
+# Create tfrecord for training partition
+cmd = "py Tensorflow/scripts/preprocessing/generate_tfrecord.py -x" + xmldirTrain + " -l " + labelsPathTrain
+cmd += " -o " + outputPathTrain + " -i " + imageDirTrain
+
 p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell = True)
 out, err = p.communicate()
+print(out)
+print(err)
